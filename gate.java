4f9fb956417b6de0df5ea58d4ba19ef6391ab936
kawai9126 <iroha.9126@gmail.com>
2021-09-10 10:55:43 +0000

    if else

---
+++ gate.java
@@ -0,0 +1,32 @@
+class Gate {
+  public static void main(String[] args) {
+    
+    if (true) {
+      System.out.println("条件式がtrueのため、出力されます");
+    }
+    
+    if (false) {
+      System.out.println("条件式がfalseのため、出力されません");
+    }
+    
+    int x = 5;
+    if (x > 2) {
+      System.out.println("xは2より大きい");
+    }
+    
+    if (x > 8) {
+      System.out.println("xは8より大きい");
+    }
+    
+    
+    int number = 12;
+    
+    if (number < 10) {
+      System.out.println("10より小さい");
+    } else if (number < 20) {
+      System.out.println("10以上、20より小さい");
+    } else {
+      System.out.println("20以上");
+    }
+  }
+}
