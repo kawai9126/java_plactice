58086192fdd9bfc2c3e0aad8d8822159c0210f0f
kawai9126 <iroha.9126@gmail.com>
2021-09-11 13:55:13 +0000

    break

--- prac.java
+++ prac.java
@@ -3,5 +3,27 @@ class Prac {
     for (int i = 1; i <= 10; i++) {
       System.out.println(i + "回目のループです");
     }
+    
+    System.out.println("=== while文 ===");
+    int i = 1;
+    while (i < 10) {
+      if (i % 5 == 0) {
+        break;
+      }
+      
+      System.out.println(i);
+      i++;
+    }
+    
+    System.out.println("=== for文 ===");
+    for (int j = 1; j < 10; j++) {
+      if (j % 3 == 0) {
+        continue;
+      }
+      
+      System.out.println(j);
+    }
+    
+    
   }
 }
