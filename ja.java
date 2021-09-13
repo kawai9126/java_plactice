38c645aae76993a6ea9ee2fc5b35199450743a03
kawai9126 <iroha.9126@gmail.com>
2021-09-13 14:04:51 +0000

    配列　for

---
+++ ja.java
@@ -0,0 +1,9 @@
+class Ja {
+  public static void main(String[] args) {
+    String[] names = {"神", "仙人", "ベイビー"};
+    
+    for (String name : names) {
+      System.out.println("私の名前は" + name + "です");
+    }
+  }
+}
