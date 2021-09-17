0f3a943c7c594cf840ca2140e24ffd94d8971724
kawai9126 <iroha.9126@gmail.com>
2021-09-17 14:40:13 +0000

    アルゴリズム勉強

---
+++ alg.java
@@ -0,0 +1,10 @@
+public class Alg {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+        String input = br.readLine();
+        String[] param = input.split(" ");
+	int num1 = Integer.parseInt(param[0]);
+    int num2 = Integer.parseInt(param[1]);
+  System.out.println(num1 + num2);
+    }
+}
